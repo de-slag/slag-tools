d0afb922a65883de8f655ff97c76899918f81ce9
de-slag <35847444+de-slag@users.noreply.github.com>
2020-05-13 06:45:09 +0200

    removed $0 echoes

--- shell-scripts/server-setup-1-01.sh
+++ shell-scripts/server-setup-1-01.sh
@@ -13,16 +13,12 @@ fi
 
 cd logic
 
-
-echo "FIXME: dont echo DOLLAR0 here"
-exit 1
-
 if [ -e minutely.sh ] ; then
   mv minutely.sh minutely.sh-BAK_$CURRENT_TS
 fi
 
 echo "#!/bin/bash" > minutely.sh
-echo "run '$0'" >> minutely.sh
+echo "run 'minutely.sh'" >> minutely.sh
 
 
 if [ -e at-startup.sh ] ; then
@@ -30,5 +26,5 @@ if [ -e at-startup.sh ] ; then
 fi
 
 echo "#!/bin/bash" > at-startup.sh
-echo "run '$0'" >> minutely.sh
+echo "run 'at-startup'" >> at-startup.sh
 
