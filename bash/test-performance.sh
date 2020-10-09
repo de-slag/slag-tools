fb2291635ac1f8181aa9c5c7acca0d4854c4129a
post@sebastianlipp.de <post@sebastianlipp.de>
2020-10-09 22:57:11 +0200

    started cpu count depending thread count

--- bash/test-performance.sh
+++ bash/test-performance.sh
@@ -1,21 +1,37 @@
 #!/bin/bash
 #set -x
 
-## target duration in seconds
 duration=5
 
-start=$(date +%s)
-target_end=$[$start+$duration]
+function test_performance {
+  start=$(date +%s)
+  target_end=$[$start+$duration]
+  echo "start performance test..."
 
-echo "start performance test..."
+  i="0"
+  while [ true ]
+  do
+    current_ts=$(date +%s)
+    if [ $current_ts -ge $target_end ] ; then
+      break;
+    fi
+    i=$[$i+1]
+  done
+  echo "test done. cycles: $i"
+  exit 0
+}
 
-i="0"
-while [ true ]
-do
-  current_ts=$(date +%s)
-  if [ $current_ts -ge $target_end ] ; then
-    break;
-  fi
-  i=$[$i+1]
-done
-echo "test done. cycles: $i"
+function start_threads {
+  cpu_count=$(nproc)
+  echo "$cpu_count cpus found"
+  threads=0
+  while [ $threads -lt $cpu_count ]
+  do
+    test_performance &
+    threads=$[$threads+1]
+  done
+}
+
+start_threads
+
+
