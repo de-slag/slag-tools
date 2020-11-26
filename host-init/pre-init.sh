beaa0d2efc32d69200c8a8dfdaaff195e6444590
de-slag <35847444+de-slag@users.noreply.github.com>
2020-11-26 16:40:01 +0100

    Update pre-init.sh

--- host-init/pre-init.sh
+++ host-init/pre-init.sh
@@ -1,1 +1,25 @@
 #!/bin/bash
+
+KEY=""
+VALUE=""
+function split_into_key_value {
+  local key_value=$1
+  if [ -z $key_value ] ; then
+    echo "key-value-string is empty"
+    exit 1
+  fi
+
+  OIFS=$IFS
+  IFS='='
+
+  for token in $key_value
+  do
+    if [ -z $KEY ] ; then
+      KEY=$token
+      continue
+    fi
+    VALUE=$token
+  done
+  IFS=$OIFS
+}
+
