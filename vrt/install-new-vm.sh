c3dd2dd21109d9f0cefcbbba43639fb591dad8f8
de-slag <35847444+de-slag@users.noreply.github.com>
2020-11-28 23:08:42 +0100

    v2

--- vrt/install-new-vm.sh
+++ vrt/install-new-vm.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# v2
 IMG=/home/vrt-test/virt-test-11-28.img
 ISO=/home/vrt-test/ubuntu-20.04.1-live-server-amd64.iso
 virt-install --connect qemu:///system -n foo -r 1024 --vcpus=1 -f $IMG -s 12 --vnc --cdrom $ISO --noautoconsole --os-type linux --accelerate  -m 00:00:00:00:00:07 -k de --vnc 
