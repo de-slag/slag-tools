1cbc93abddc18b7f3cfffdc2ae44aff747d6dc32
de-slag <35847444+de-slag@users.noreply.github.com>
2020-11-28 23:14:24 +0100

    Update install-new-vm.sh

--- vrt/install-new-vm.sh
+++ vrt/install-new-vm.sh
@@ -2,5 +2,10 @@
 # v2
 IMG=/home/vrt-test/virt-test-11-28.img
 ISO=/home/vrt-test/ubuntu-20.04.1-live-server-amd64.iso
-virt-install --connect qemu:///system -n foo -r 1024 --vcpus=1 -f $IMG -s 12 --vnc --cdrom $ISO --noautoconsole --os-type linux --accelerate  -m 00:00:00:00:00:07 -k de --vnc 
+NAME=foo
+
+# kernel panic at 512 
+RAM=1024
+
+virt-install --connect qemu:///system -n $NAME -r $RAM --vcpus=1 -f $IMG -s 12 --vnc --cdrom $ISO --noautoconsole --os-type linux --accelerate  -m 00:00:00:00:00:07 -k de --vnc 
 #--network=bridge:br0,model=virtio
