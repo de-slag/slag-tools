b2e14905dc61a0e2e8c1b8368169a2a7be46d199
de-slag <35847444+de-slag@users.noreply.github.com>
2020-11-30 15:46:14 +0100

    Update rename-host.sh

--- host/rename-host.sh
+++ host/rename-host.sh
@@ -3,12 +3,28 @@
 source ~/slag-tools/utils/base-utils-stable.sh
 source ~/slag-tools/utils/logging-utils-stable.sh
 
-TARGET_HOSTNAME=$1
+CURRENT_HOSTNAME=$1
+TARGET_HOSTNAME=$2
 TIMESTAMP=$(date +%s)
+
+SED_STRING='s/$CURRENT_HOSTNAME/$TARGET_HOSTNAME/'
+
 
 function create_backups {
   cp /etc/hostname /etc/hostname.bak.$TIMESTAMP
   cp /etc/hosts /etc/hosts.bak.$TIMESTAMP
 }
 
+function sed_hostname {
+  sed '$SED_STRING /etc/hostname
+}
 
+function sed_hosts {
+  sed '$SED_STRING /etc/hosts
+}
+
+create_backups
+sed_hosts
+sed_hostname
+hostname -F /etc/hostname
+reboot
