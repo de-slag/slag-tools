8a5e8ea84b6b87f886f73371945b1ad8cca2d185
de-slag <35847444+de-slag@users.noreply.github.com>
2020-11-30 13:54:34 +0100

    Create global-config-utils-test.sh

---
+++ utils/global-config-utils-test.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+source logging-utils-stable.sh
+
+if [ "$1" = "LATEST" ] ; then
+  source global-config-utils-latest.sh
+else
+  source global-config-utils-stable.sh
+fi
+
+function assert_equals {
+  local message=$1
+  local expected=$2
+  local current=$3
+  if [ "$expected" != "$current" ] ; then
+    echo "not equal. expected $expected, current $current"  
+    return
+  fi
+  echo "ok: $message"
+}
+
+read_config_value
+assert_equals "no key" "" $CONFIG_VALUE
+
+read_config_value test.lorem.ipsum
+assert_equals "lorem ipsum" "Lorem ipsum dolor sit amet." $CONFIG_VALUE
+
