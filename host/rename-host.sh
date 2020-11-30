08064da57a7074b64cd2867d5ad89d55bbba1d38
de-slag <35847444+de-slag@users.noreply.github.com>
2020-11-30 12:59:13 +0100

    Update rename-host.sh

--- host/rename-host.sh
+++ host/rename-host.sh
@@ -4,6 +4,11 @@ source ~/slag-tools/utils/base-utils-stable.sh
 source ~/slag-tools/utils/logging-utils-stable.sh
 
 TARGET_HOSTNAME=$1
+TIMESTAMP=$(date +%s)
 
-log_info "target hostname: $TARGET_HOSTNAME"
+function create_backups {
+  cp /etc/hostname /etc/hostname.bak.$TIMESTAMP
+  cp /etc/hosts /etc/hosts.bak.$TIMESTAMP
+}
+
 
