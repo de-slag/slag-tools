dc0f425d5d092ca22e528a32ea3362885b94c685
de-slag <35847444+de-slag@users.noreply.github.com>
2020-11-30 12:29:05 +0100

    Update rename-host.sh

--- host/rename-host.sh
+++ host/rename-host.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 source ~/slag-tools/utils/base-utils-stable.sh
-source ~/slag-tools/utils/log-utils-stable.sh
+source ~/slag-tools/utils/logging-utils-stable.sh
 
 TARGET_HOSTNAME=$1
 
