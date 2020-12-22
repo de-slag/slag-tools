bcc83af65b693d4a565f9c371b02066a20d733f8
Sebastian Lipp <post@sebastianlipp.de>
2020-12-22 10:05:36 +0100

    a demo to show how to replace a substring in an string
    demo for nfs mounting logic

--- bash/demo/replace-string-demo.sh
+++ bash/demo/replace-string-demo.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
 # demo 1
-firstString="I love Suzi and Marry"
-secondString="Sara"
-echo "${firstString/Suzi/$secondString}"
 
+function demo1 {
+  firstString="I love Suzi and Marry"
+  echo $firstString
+  secondString="Sara"
+  echo "${firstString/Suzi/$secondString}"
+}
 
+# demo 2
 
 uranus="uranus.speedport.ip"
 jupiter="jupiter.speedport.ip"
-
 HOST_REPLACED=
+
 function replace_host {
   source_string="$1"
   HOST_REPLACED=
@@ -24,14 +28,28 @@ function replace_mnt {
   MNT_REPLACED="${source_string/mnt/media}"
 }
 
-
-# demo 2
-echo "demo 2..."
-input=~/slag-configurations/fstab.dat
-while IFS= read -r line
-do
-  replace_host "$line"
-  replace_mnt "$HOST_REPLACED"
+function demo2 {
+  echo "demo 2..."
+  input=~/slag-configurations/fstab.dat
+  while IFS= read -r line
+  do
+    replace_host "$line"
+    replace_mnt "$HOST_REPLACED"
   echo "$MNT_REPLACED"
-done < $input
+  done < $input
+}
 
+# demo 3
+function demo3 {
+
+  # this also runs with '&' instead of  '§'
+  # this does not runs with '$', '#', '%'
+
+  local firstString="this ist a '§xvar' test"
+  echo "${firstString/§xvar/nice}"
+  echo "${firstString/§xvar/fancy}"
+}
+
+demo1
+demo2
+demo3
