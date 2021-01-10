efd753eeda11a1f636a990cbfdf790d6b5563992
post@sebastianlipp.de <post@sebastianlipp.de>
2021-01-10 15:28:18 +0100

    print pdfs and archive - minor changes

--- bash/print-pdf.sh
+++ bash/print-pdf.sh
@@ -4,10 +4,10 @@ set -euo pipefail
 PRINTER_NAME=Kyocera_FS-1030D
 TO_PRINT_DIR=/var/spool/lpd
 TS=$(date '+%Y-%m-%d_%H-%M-%S')
-LOG_FILE=print.log
+LOG_FILE=$TO_PRINT_DIR/print.log
 
-echo $TS                   >> $LOG_FILE
 cd $TO_PRINT_DIR           >> $LOG_FILE
+echo $TS                   >> $LOG_FILE
 lp -d $PRINTER_NAME *.pdf  >> $LOG_FILE 
 tar -cvzf $TS.tar.gz *.pdf >> $LOG_FILE
 rm -Rv *.pdf               >> $LOG_FILE
