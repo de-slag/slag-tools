87a6af71c54f362e07092324c509b3be343c2efe
post@sebastianlipp.de <post@sebastianlipp.de>
2021-01-10 15:21:54 +0100

    print pdfs and archive - minor changes

--- bash/print-pdf.sh
+++ bash/print-pdf.sh
@@ -6,7 +6,8 @@ TO_PRINT_DIR=/var/spool/lpd
 TS=$(date '+%Y-%m-%d_%H-%M-%S')
 LOG_FILE=print.log
 
-cd $TO_PRINT_DIR
-
-lp -d $PRINTER_NAME *.pdf >> $LOG_FILE 
+echo $TS                   >> $LOG_FILE
+cd $TO_PRINT_DIR           >> $LOG_FILE
+lp -d $PRINTER_NAME *.pdf  >> $LOG_FILE 
 tar -cvzf $TS.tar.gz *.pdf >> $LOG_FILE
+rm -Rv *.pdf               >> $LOG_FILE
