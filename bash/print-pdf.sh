d457de3cf300a42c1958fc3f51a676cb4a7c3064
post@sebastianlipp.de <post@sebastianlipp.de>
2021-01-10 15:33:50 +0100

    print pdfs: error out to std out

--- bash/print-pdf.sh
+++ bash/print-pdf.sh
@@ -8,6 +8,6 @@ LOG_FILE=$TO_PRINT_DIR/print.log
 
 cd $TO_PRINT_DIR           >> $LOG_FILE
 echo $TS                   >> $LOG_FILE
-lp -d $PRINTER_NAME *.pdf  >> $LOG_FILE 
-tar -cvzf $TS.tar.gz *.pdf >> $LOG_FILE
-rm -Rv *.pdf               >> $LOG_FILE
+lp -d $PRINTER_NAME *.pdf  &>> $LOG_FILE 
+tar -cvzf $TS.tar.gz *.pdf &>> $LOG_FILE
+rm -Rv *.pdf               &>> $LOG_FILE
