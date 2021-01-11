563bb08248a26bda90d569e4c81063c19fc99c55
post@sebastianlipp.de <post@sebastianlipp.de>
2021-01-11 19:50:44 +0100

    TLS-X: refactoring print-pdf

---
+++ bash/def-print-pdf.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+~/slag-tools/bash/print-pdf.sh DEBUG -p=Kyocera_FS-1030D -d=/var/spool/lpd
--- bash/print-pdf.sh
+++ bash/print-pdf.sh
@@ -1,13 +1,34 @@
 #!/bin/bash
-set -euo pipefail
 
-PRINTER_NAME=Kyocera_FS-1030D
-TO_PRINT_DIR=/var/spool/lpd
-TS=$(date '+%Y-%m-%d_%H-%M-%S')
-LOG_FILE=$TO_PRINT_DIR/print.log
+source ~/slag-tools/bash/core-script.sh
 
-cd $TO_PRINT_DIR           >> $LOG_FILE
-echo $TS                   >> $LOG_FILE
-lp -d $PRINTER_NAME *.pdf  &>> $LOG_FILE 
-tar -cvzf $TS.tar.gz *.pdf &>> $LOG_FILE
-rm -Rv *.pdf               &>> $LOG_FILE
+for i in "$@" ; do
+  case $i in
+    -p=*|--printer-name=*)
+      PRINTER_NAME="${i#*=}"
+      shift # past argument=value
+      ;;
+    -d=*|--dir-to-print=*)
+      TO_PRINT_DIR="${i#*=}"
+      shift # past argument=value
+      ;;
+    *)
+      log_warn "unknown option: $i"
+    ;;
+  esac
+done
+
+readonly TS=$(date '+%Y-%m-%d_%H-%M-%S')
+
+log_debug "printer name: $PRINTER_NAME"
+log_debug "to print dir: $TO_PRINT_DIR"
+log_debug "timestamp: $TS"
+
+log_info "start"
+
+cd $TO_PRINT_DIR
+lp -d $PRINTER_NAME *.pdf
+tar -cvzf $TS.tar.gz *.pdf
+rm -Rv *.pdf
+
+
