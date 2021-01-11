57a8858257dd987c2bb9b992d621285b28f8a605
post@sebastianlipp.de <post@sebastianlipp.de>
2021-01-11 20:28:10 +0100

    TLS-X: refactoring print-pdf
    added logging stuff

--- bash/def-print-pdf.sh
+++ bash/def-print-pdf.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-~/slag-tools/bash/print-pdf.sh DEBUG -p=Kyocera_FS-1030D -d=/var/spool/lpd
+~/slag-tools/bash/print-pdf.sh DEBUG -p=Kyocera_FS-1030D -d=/var/spool/lpd &>> /var/log/slag/print-pdf.log
