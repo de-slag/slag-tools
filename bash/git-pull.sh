c4f028012ac668c1d95aaf900a39857618e326f8
post@sebastianlipp.de <post@sebastianlipp.de>
2021-01-28 06:56:53 +0100

    git-pull on multiple repos on multiple branches

--- bash/git-pull.sh
+++ bash/git-pull.sh
@@ -1,13 +1,35 @@
 #!/bin/bash
 
-cd ~
-echo "pull slag-configurations..."
-cd slag-configurations
-git pull
-echo "pull slag-configurations. done."
-cd ..
+repositories="slag-configurations slag-tools"
+branches="develop master"
 
-echo "pull slag-tools..."
-cd slag-tools
-git pull
-echo "pull slag-tools. done."
+function pull_repo_branch {
+  local repo=$1
+  local branch=$2
+  echo "pulling $repo $branch"
+
+  cd ~
+  if [ ! -d $repo ] ; then
+    echo "repo not found: '$repo'. return."
+    return
+  fi
+  cd $repo
+  local branch_info=$(git branch | grep $branch)
+  if [ -z "$branch_info" ] ; then
+     echo "branch not found: '$branch' on repo '$repo'. return."
+    return 
+  fi
+  git checkout $branch
+  git pull
+}
+
+function pull_repo {
+  local repo=$1
+  for branch in $branches ; do
+    pull_repo_branch $repo $branch
+  done
+}
+
+for repo in $repositories ; do
+  pull_repo $repo
+done
